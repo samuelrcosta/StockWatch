3d636f9457e253bc7cd410a6d1cd8272743fce62
Samuel Rocha Costa <samu.rcosta@gmail.com>
2019-05-21 08:17:41 -0300

    Getting connection from pool

--- app/models/FavoriteStock.js
+++ app/models/FavoriteStock.js
@@ -1,48 +1,54 @@
 const db = require('./../../config/dbConnection');
 
 class FavoriteStock{
-  constructor(){
-    this._connection = db;
-  }
-  
+
   getList(){
     return new Promise((resolve, reject) => {
-      this._connection.query('select * from favorite_stock order by name asc', function (err, rows) {
-        // When done with the connection, release it.
-        this._connection.release();
+      db.getConnection(function(err, connection) {
+        if(err) throw err;
+        connection.query('select * from favorite_stock order by name asc', function (err, rows) {
+          // When done with the connection, release it.
+          connection.release();
 
-        if (err) {
-          return reject(err);
-        }
-        resolve(rows);
+          if (err) {
+            return reject(err);
+          }
+          resolve(rows);
+        });
       });
     });
   };
   
   insert(stock){
     return new Promise((resolve, reject) => {
-      this._connection.query('insert into favorite_stock set ? ', stock, function (err, rows) {
-        // When done with the connection, release it.
-        this._connection.release();
-        
-        if (err) {
-          return reject(err);
-        }
-        resolve(rows);
+      db.getConnection(function(err, connection) {
+        if(err) throw err;
+        connection.query('insert into favorite_stock set ? ', stock, function (err, rows) {
+          // When done with the connection, release it.
+          connection.release();
+          
+          if (err) {
+            return reject(err);
+          }
+          resolve(rows);
+        });
       });
     });
   }
   
   delete(symbol){
     return new Promise((resolve, reject) => {
-      this._connection.query('delete from favorite_stock where symbol = ? ', symbol, function (err, rows) {
-        // When done with the connection, release it.
-        this._connection.release();
+      db.getConnection(function(err, connection) {
+        if(err) throw err;
+        connection.query('delete from favorite_stock where symbol = ? ', symbol, function (err, rows) {
+          // When done with the connection, release it.
+          connection.release();
 
-        if (err) {
-          return reject(err);
-        }
-        resolve(rows);
+          if (err) {
+            return reject(err);
+          }
+          resolve(rows);
+        });
       });
     });
   }
